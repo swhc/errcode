7bc7c7a174d2afa012ac6b56a02debdb8fec24be
steven <steven>
2021-11-16 17:17:57 +0800

    feat(errormap):  first commit

---
+++ doc.go
@@ -0,0 +1,10 @@
+// 每一个文件代表一个服务
+// StatusOK                           = 200 // RFC 7231, 6.3.1
+// StatusBadRequest                   = 400 // RFC 7231, 6.5.1
+// StatusUnauthorized                 = 401 // RFC 7235, 3.1
+// StatusForbidden                    = 403 // RFC 7231, 6.5.3
+// StatusNotFound                     = 404 // RFC 7231, 6.5.4
+// StatusInternalServerError          = 500 // RFC 7231, 6.6.1
+package errors_map
+
+
---
+++ example.go
@@ -0,0 +1,27 @@
+package errors_map
+
+//go:generate codegen -type=int
+
+// 基础服务 : user errors.
+const (
+	// ErrUserNotFound - 404: User not found.
+	ErrUserNotFound int = iota + 110001
+
+	// ErrUserAlreadyExist - 400: User already exist.
+	ErrUserAlreadyExist
+)
+
+// 基础服务: secret errors.
+const (
+	// ErrEncrypt - 400: Secret reach the max count.
+	ErrReachMaxCount int = iota + 110101
+
+	//  ErrSecretNotFound - 404: Secret not found.
+	ErrSecretNotFound
+)
+
+// 基础服务: policy errors.
+const (
+	// ErrPolicyNotFound - 404: Policy not found.
+	ErrPolicyNotFound int = iota + 110201
+)
