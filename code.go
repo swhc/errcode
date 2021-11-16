9da3e34a8953068a0cd690aa9a58407e82314bd9
steven <steven>
2021-11-16 19:57:51 +0800

    feat(sdk):  init code

--- code.go
+++ code.go
@@ -1,20 +1,6 @@
 package errors_map
 
-import (
-	"github.com/mongofs/errors"
-)
-
 func init() {
-	errors.Register(ErrSdkMatchNotExist, 400, "match not found.")
-	errors.Register(ErrSdkMatchLineupNotData, 400, "lineup no data.")
-	errors.Register(ErrSdkDataNotExist, 400, "no data.")
-	errors.Register(ErrSdkTeamNotExist, 400, "team not found.")
-	errors.Register(ErrSdkPlayerNotExist, 400, "player not found.")
-
-	errors.Register(ErrSdkSystemDatabaseErr, 500, "database err")
-	errors.Register(ErrSdkSystemRedisErr, 500, "redis err")
-	errors.Register(ErrSdkSystemJsonEncodeErr, 500, "json encode err")
-	errors.Register(ErrSdkSystemJsonDecodeErr, 500, "json decode err")
-	errors.Register(ErrSdkSystemProtoEncodeErr, 500, "proto encode err")
-	errors.Register(ErrSdkSystemProtoDecodeErr, 500, "proto decode err")
+	// init register sdk code
+	initSdk()
 }
--- sdk.go
+++ sdk.go
@@ -2,6 +2,8 @@
 
 package errors_map
 
+import "github.com/mongofs/errors"
+
 // sdk : match
 const (
 	// ErrSdkMatchNotExist - 400: match not found.
@@ -46,3 +48,19 @@ const (
 	// ErrSdkSystemProtoDecodeErr - 500: proto decode err
 	ErrSdkSystemProtoDecodeErr
 )
+
+
+func initSdk(){
+	errors.Register(ErrSdkMatchNotExist, 400, "match not found.")
+	errors.Register(ErrSdkMatchLineupNotData, 400, "lineup no data.")
+	errors.Register(ErrSdkDataNotExist, 400, "no data.")
+	errors.Register(ErrSdkTeamNotExist, 400, "team not found.")
+	errors.Register(ErrSdkPlayerNotExist, 400, "player not found.")
+
+	errors.Register(ErrSdkSystemDatabaseErr, 500, "database err")
+	errors.Register(ErrSdkSystemRedisErr, 500, "redis err")
+	errors.Register(ErrSdkSystemJsonEncodeErr, 500, "json encode err")
+	errors.Register(ErrSdkSystemJsonDecodeErr, 500, "json decode err")
+	errors.Register(ErrSdkSystemProtoEncodeErr, 500, "proto encode err")
+	errors.Register(ErrSdkSystemProtoDecodeErr, 500, "proto decode err")
+}
