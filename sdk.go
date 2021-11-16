a94708133d79d0f98840ba17bdebcc7d0931c1f7
steven <steven>
2021-11-16 19:28:07 +0800

    feat(errormap):  抽离init 到code 文件

--- sdk.go
+++ sdk.go
@@ -47,17 +47,3 @@ const (
 	ErrSystemProtoDecodeErr
 )
 
-func init() {
-	errors.Register(ErrMatchNotExist, 400, "match not found.")
-	errors.Register(ErrMatchLineupNotData, 400, "lineup no data.")
-	errors.Register(ErrDataNotExist, 400, "no data.")
-	errors.Register(ErrTeamNotExist, 400, "team not found.")
-	errors.Register(ErrPlayerNotExist, 400, "player not found.")
-
-	errors.Register(ErrSystemDatabaseErr, 500, "database err")
-	errors.Register(ErrSystemRedisErr, 500, "redis err")
-	errors.Register(ErrSystemJsonEncodeErr, 500, "json encode err")
-	errors.Register(ErrSystemJsonDecodeErr, 500, "json decode err")
-	errors.Register(ErrSystemProtoEncodeErr, 500, "proto encode err")
-	errors.Register(ErrSystemProtoDecodeErr, 500, "proto decode err")
-}
